d1687a504dc4e4e17f813f41318063acb6886aa5
Viktor Marynych <crueltyfrizz@gmail.com>
2024-06-21 01:32:55 +0300

    Add +,-,*,/ operations Fixed memory bug with reallocate

--- main.c
+++ main.c
@@ -6,11 +6,24 @@ int main(void) {
 
     Chunk chunk;
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 12);
-    writeChunk(&chunk, constant, 13);
-    writeChunk(&chunk, OP_NEGATE, 14);
-    writeChunk(&chunk, OP_RETURN, 15);
+    int constant = addConstant(&chunk, 10);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_RETURN, 17);
 
     interpret(&chunk);
     freeVM();
--- vm.c
+++ vm.c
@@ -23,6 +23,12 @@ static InterpretResult  run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+ do {                 \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+ } while(false)
 
     for(;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -42,9 +48,11 @@ static InterpretResult  run()
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
-            case OP_NEGATE:
-                push(-pop());
-                break;
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:
                 printValue(pop());
                 printf("\n");
@@ -54,9 +62,9 @@ static InterpretResult  run()
         }
     }
 
-    return INTERPRET_OK;
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
